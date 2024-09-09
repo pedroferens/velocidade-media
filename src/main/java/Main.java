ba6b0db71658876b3589138f955a9bfed7f750fc
pedroferens <169068429+pedroferens@users.noreply.github.com>
2024-09-09 10:59:00 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("calcular a velocidade media em km/h");
+    System.out.println();
+    System.out.println("A distância considerada é");
+    double distancia = 400;
+    System.out.println(distancia);
+    System.out.println();
+    System.out.println("o tempo percorido");
+    double tempo = 3;
+    System.out.println(tempo);
+    System.out.println();
+    System.out.println("velocidade media");
+    double velocidadeMedia = distancia / tempo; 
+    System.out.println(velocidadeMedia);
+      
+  }
+
+  
+}
